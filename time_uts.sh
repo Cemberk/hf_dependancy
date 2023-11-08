92ed6c414e81c026d7d44d556f67f908630000d5
root <root@rocm-framework-a100-1.amd.com>
2023-11-08 19:10:22 +0000

    log file

--- time_uts.sh
+++ time_uts.sh
@@ -3,7 +3,7 @@
 echo "Workload started..."
 mytime="$(time ( RUN_SLOW=1 pytest tests/ > all_test.log ) 2>&1 1>/dev/null )"
 
-echo $mytime
+echo $mytime > time.log
 
 # Extract the real time from the time command output and save it to the TIME_FILE
-grep real $mytime | awk '{print $2}' > time_output.txt
+grep real time.log | awk '{print $2}' > time_output.txt
