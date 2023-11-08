41ebffc15f440e92821beb06cfaff5d5bd4365be
Cemberk <Cem.bozkus@amd.com>
2023-11-08 12:23:57 -0600

    Update time_uts.sh

--- time_uts.sh
+++ time_uts.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-# File to store the execution time
-TIME_FILE="time_output.txt"
+echo "Workload started..."
+mytime="$(time ( RUN_SLOW=1 pytest tests/ > all_test.log ) 2>&1 1>/dev/null )"
 
-# The workload command
-WORKLOAD_COMMAND="RUN_SLOW=1 pytest tests/ 2>&1 | tee all_test.log"
+echo $mytime
 
-# Function to execute the workload and capture the time
-run_and_capture_time() {
-    # Use the time command and redirect the stderr to stdout to capture the time output
-    { time $WORKLOAD_COMMAND ; } 2>&1 | grep real | awk '{print $2}' > $TIME_FILE
-}
-
-# Export the TIME_FILE variable so it's available in the subshell
-export TIME_FILE
-
-# Export the WORKLOAD_COMMAND variable so it's available in the subshell
-export WORKLOAD_COMMAND
-
-# Run the workload in the background and capture the time
-run_and_capture_time &
-
-echo "Workload started in the background..."
+# Extract the real time from the time command output and save it to the TIME_FILE
+grep real $mytime | awk '{print $2}' > time_output.txt
